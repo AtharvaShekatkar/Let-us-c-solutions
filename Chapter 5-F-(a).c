fa925c1d9c3da58beca95a91f243bf37fbfc5847
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-08-25 23:32:55 +0530

    Create Chapter 5-F-(a).c

---
+++ Chapter 5-F-(a).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float product(float, int);
+int main(){
+	float num1, prod;
+	int num2;
+	printf("Enter a number: ");
+	scanf("%f", &num1);
+	printf("Enter another number: ");
+	scanf("%d", &num2);
+	prod = product(num1, num2);
+	printf("product of %f and %d is = %f", num1, num2, prod);
+	return 0;
+}
+float product(float a, int b){
+	float prod;
+	prod = a * b;
+	return prod;
+}
