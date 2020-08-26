2dc203fa8675c50403e9b92ec1dee8db537ce877
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-08-26 12:20:12 +0530

    Create Chapter 5-J-(a)-1.c

---
+++ Chapter 5-J-(a)-1.c
@@ -0,0 +1,18 @@
+//A 5-digit positive integer is entered through the keyboard, write a function to calculate sum of digits of the 5-digit number: without using recursion
+int sumofdigits(int);
+int main(){
+	int num, sum;
+	printf("Enter a 5 digit number: ");
+	scanf("%d", &num);
+	sum = sumofdigits(num);
+	printf("Sum = %d", sum);
+	return 0;
+}
+sumofdigits(int num){
+	int sum = 0, i;
+	for(i = 1; i <= 5; i++){
+		sum = sum + (num % 10);
+		num /= 10;
+	}
+	return sum;
+}
