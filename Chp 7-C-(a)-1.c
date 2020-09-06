097bb353c2aaa1e1eb7590c112f1b712043fe5b2
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-09-06 17:49:52 +0530

    Create Chp 7-C-(a)-1.c

---
+++ Chp 7-C-(a)-1.c
@@ -0,0 +1,16 @@
+Write down macro definitions for the following: To test whether a character entered is a small case letter or not.
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+#define TESTSMALLCASE (c >= 97 && c <= 122)
+int main(){
+    char c;
+    printf("Enter a letter from the english alphabet: ");
+    scanf("%c", &c);
+    if(TESTSMALLCASE)
+        printf("It's a smallcase letter!");
+    else
+        printf("It's not a smallcase letter.");
+    getch();
+    return 0;
+}
