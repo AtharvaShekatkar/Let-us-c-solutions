3848feea286a614a6bd7755455f09312dc37a20d
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-09-06 17:52:46 +0530

    Create Chp 7-C-(a)-2.c

---
+++ Chp 7-C-(a)-2.c
@@ -0,0 +1,16 @@
+Write down macro definitions for the following: To test whether a character entered is a upper case letter or not.
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+#define TESTUPPERCASE (c >= 65 && c <= 90)
+int main(){
+    char c;
+    printf("Enter a letter from the english alphabet: ");
+    scanf("%c", &c);
+    if(TESTUPPERCASE)
+        printf("It's a uppercase letter!");
+    else
+        printf("It's not a uppercase letter.");
+    getch();
+    return 0;
+}
