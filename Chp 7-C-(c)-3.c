c6d3e170fc2cb40c5a3a18c736ab300f6f93acb1
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-09-06 19:18:04 +0530

    Create Chp 7-C-(c)-3.c

---
+++ Chp 7-C-(c)-3.c
@@ -0,0 +1,14 @@
+Write down macro definitions for the following: To convert a uppercase alphabet to lowercase.
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+#define UPPERTOLOWER(ch) (ch = (ch - 65) + 97)    //using ascii values of upper case and smaller case letters 
+int main(){
+    char c;
+    printf("Enter an uppercase letter: ");
+    scanf("%c", &c);
+    UPPERTOLOWER(c);
+    printf("%c\n", c);
+    getch();
+    return 0;
+}
