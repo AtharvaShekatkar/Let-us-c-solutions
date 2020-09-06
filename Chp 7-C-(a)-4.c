0831c0ee31a60413030a84e101ec853bcb87efe1
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-09-06 17:59:48 +0530

    Create Chp 7-C-(a)-4.c

---
+++ Chp 7-C-(a)-4.c
@@ -0,0 +1,18 @@
+Write macro definitions for the following: To obtain the bigger of two numbers.
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+#define BIGGER(a, b) (a > b)      //can have a macro without arguments as well
+int main(){
+    int num1, num2;
+    printf("Enter a number: ");
+    scanf("%d", &num1);
+    printf("Enter another number: ");
+    scanf("%d", &num2);
+    if(BIGGER(num1, num2))
+        printf("%d is bigger", num1);
+    else
+        printf("%d is bigger", num2);
+    getch();
+    return 0;
+}
