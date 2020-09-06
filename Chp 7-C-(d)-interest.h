4c214b809e6289c916dab1efa9d2c3a3fe722c6b
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-09-06 17:39:05 +0530

    Create Chp 7-C-(d)-interest.h
    Extra file as required in the question

---
+++ Chp 7-C-(d)-interest.h
@@ -0,0 +1,3 @@
+Write macro definitions with arguments for calculation of Simple Interest and Amount. Store these macro definitions in a file called “interest.h”. Include this file in your program, and use the macro definitions for calculating simple interest and amount.
+#define INTEREST(p, n, r) ((p * n * r) / 100)
+#define AMOUNT(p, si) (p + si)
