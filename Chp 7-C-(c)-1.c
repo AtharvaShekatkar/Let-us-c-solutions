a41293d33dffb1d50826744a1f309b953662adfa
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2020-09-06 18:17:11 +0530

    Create Chp 7-C-(c)-1.c

---
+++ Chp 7-C-(c)-1.c
@@ -0,0 +1,17 @@
+Write down macro definitions for the following: To find arithmetic mean of two numbers.
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+#define ARMEAN(x, y) ((x + y) / 2.0)
+int main(){
+    int num1, num2;
+    float mean;
+    printf("Enter a number: ");
+    scanf("%d", &num1);
+    printf("Enter another number: ");
+    scanf("%d", &num2);
+    mean = ARMEAN(num1, num2);
+    printf("Arithmetic Mean of %d and %d = %f", num1, num2, mean);
+    getch();
+    return 0;
+}
