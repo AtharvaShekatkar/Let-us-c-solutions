63aaf282d3e375e7955f6e3ec95a2503fb908dd1
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2021-01-07 23:41:39 +0530

    Update D-(d).c

--- chapter 9/D-(d).c
+++ chapter 9/D-(d).c
@@ -1,3 +1,7 @@
+//Write a program that extracts part of the given string from the specified position. 
+//For example, if the sting is "Working with strings is fun", then if from position 4, 4 characters are to be extracted then the program should return string as "king".
+// Moreover, if the position from where the string is to be extracted is given
+//and the number of characters to be extracted is 0 then the program should extract entire string from the specified position.
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
