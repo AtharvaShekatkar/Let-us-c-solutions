da6fdf6f862adaaa9a69f8cf572d97774287c6f5
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2021-01-08 12:50:49 +0530

    Update J-(f).c

--- Chapter 5/J-(f).c
+++ Chapter 5/J-(f).c
@@ -1,4 +1,4 @@
-Write a C function to evaluate the series: sin(x) = x - (x^3 / 3!) + (x^5 / 5!) - (x^7 / 7!) ..... upto five significant figures.
+//Write a C function to evaluate the series: sin(x) = x - (x^3 / 3!) + (x^5 / 5!) - (x^7 / 7!) ..... upto five significant figures.
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
