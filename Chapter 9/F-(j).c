17388d7b101769da251b77d10cc10c5ba17b1e22
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2021-02-24 17:25:56 +0530

    Create F-(j).c

---
+++ Chapter 9/F-(j).c
@@ -0,0 +1,52 @@
+//Write a program that takes a set of names of individuals and abbreviates the first, middle and other names except the last name by their first letter.
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 80
+
+void abbreviate(const char *,const char *,const char *, char *);
+
+int main(){
+    char n[25];
+
+    printf("Enter first name: ");
+    fgets(n, 25, stdin);
+    n[strlen(n) - 1] = 0;
+
+    char *first = (char *) malloc(strlen(n) * sizeof(char));
+    strcpy(first, n);
+
+    printf("Enter middle name: ");
+    fgets(n, 25, stdin);
+    n[strlen(n) - 1] = 0;
+
+    char *mid = (char *) malloc(strlen(n) * sizeof(char));
+    strcpy(mid, n);
+    
+    printf("Enter last name: ");
+    fgets(n, 25, stdin);
+
+    char *last = (char *) malloc(strlen(n) * sizeof(char));
+    strcpy(last, n);
+
+    char *abbreviation = (char *) malloc(strlen(last) + 6);
+
+    abbreviate(first, mid, last, abbreviation);
+
+    printf("Abbreviated name is: %s", abbreviation);
+    return 0;
+}
+
+void abbreviate(const char *first,const char *mid,const char *last,char *ab){
+    *ab = *first;
+    *(++ab) = '.';
+    *(++ab) = ' ';
+
+    *(++ab) = *mid;
+    *(++ab) = '.';
+    *(++ab) = ' ';
+    ab++;
+
+    strcpy(ab, last);
+}
