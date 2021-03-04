5cde513978993051b52e2a18d3a56c67e3c6b7b7
kuuhakublank00 <70227927+kuuhakublank00@users.noreply.github.com>
2021-03-04 23:29:20 +0530

    Update C-(d).c

--- Chapter 12/C-(d).c
+++ Chapter 12/C-(d).c
@@ -1,3 +1,6 @@
+//Suppose a file contains student’s records with each record containing name and age of a student. 
+//Write a program to read these records and display them in sorted order by name.
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
