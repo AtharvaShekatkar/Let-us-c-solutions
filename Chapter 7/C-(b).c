ebc5e5e301158b61bd1ca7aee57298b522b17a94
AtharvaShekatkar <70227927+AtharvaShekatkar@users.noreply.github.com>
2021-06-20 15:14:34 +0530

    Update C-(b).c

--- Chapter 7/C-(b).c
+++ Chapter 7/C-(b).c
@@ -1,4 +1,7 @@
-//Write macro definitions with arguments for calculation of area and perimeter of a triangle, a square and a circle. Store these macro definitions in a file called “areaperi.h”. Include this file in your program, and call the macro definitions for calculating area and perimeter for different squares, triangles and circles.
+//Write macro definitions with arguments for calculation of area and perimeter of a triangle, a square and a circle. 
+//Store these macro definitions in a file called “areaperi.h”. Include this file in your program,
+//and call the macro definitions for calculating area and perimeter for different squares, triangles and circles.
+
 #include<math.h>
 #include<stdio.h>
 #include<conio.h>
